a886194faed935126118e8a55bc422c53578039d
Anish Pradhan <anishpradha5523@gmail.com>
2023-12-24 20:28:25 +0530

    Ecommerce Website

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import NavRoutes from './Routes/NavRoutes'
 import Footer from './Component/UI/Components/Footer'
 import { BrowserRouter } from 'react-router-dom'
+import axios from 'axios'
+import Store from './Component/Content/Store'
+import { Circles } from 'react-loader-spinner'
 
 const App = () => {
-  return (
-    <>
-      <BrowserRouter>
-        <NavRoutes/>
-        <Footer/>
-      </BrowserRouter>
-    </>
-  )
+  const [data , setData] = useState([])
+
+    useEffect(()=>{
+        axios.get("https://ecommerce-backend-s1ya.onrender.com/api/findData")
+        .then((res)=>setData(res.data))
+        .catch((err)=>console.log("catch error"+err))
+    },[])
+
+    const [Data] = useState(Store)
+    console.log(data);
+    if(data.length === 0){
+      return(
+        <div style={{height:"100vh" , display:"flex", justifyContent:"center", alignItems:"center" , backgroundColor:"#e2e7f0"}}>
+          <Circles
+          height="100"
+          width="100"
+          color="#1f7678"
+          ariaLabel="circles-loading"
+          wrapperStyle={{}}
+          wrapperClass=""
+          visible={true}
+          />
+          </div>
+      )
+    }
+    else{
+      return (
+        <div>
+          <BrowserRouter>
+          <Data.Provider value={data}>
+            <NavRoutes/>
+            <Footer/>
+          </Data.Provider>
+          </BrowserRouter>
+        </div>
+      )
+    }
 }
 
 export default App
--- src/Routes/NavRoutes.jsx
+++ src/Routes/NavRoutes.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import {Route, Routes } from 'react-router-dom'
 import Home from '../Component/UI/Components/Home'
 import Furniture from '../Component/UI/Components/Furniture'
@@ -6,12 +6,10 @@ import Fashion from '../Component/UI/Components/Fashion'
 import MobileTablet from '../Component/UI/Components/MobileTablet'
 import Electronics from '../Component/UI/Components/Elctronics'
 import Navbar from '../Component/UI/Navbar'
-import Store from '../Component/Content/Store'
 import Cart from '../Component/UI/Components/Cart'
 import Authentication from '../Component/Authentication/Authentication'
 import RenderCArd from '../Component/UI/Components/RenderCArd'
 import Subcomp from '../Component/UI/Subcomp'
-import axios from 'axios'
 import Singup from '../Component/Authentication/Singup '
 import Login from '../Component/Authentication/Login'
 import SearchRender from '../Component/UI/Components/Searchrender'
@@ -19,57 +17,19 @@ import Success from '../Component/UI/Components/Stripe/Success'
 import Cancel from '../Component/UI/Components/Stripe/Cancel'
 
 const NavRoutes = () => {
-    const [data , setData] = useState([])
-
-    useEffect(()=>{
-        axios.get("https://ecommerce-backend-s1ya.onrender.com/api/findData")
-        .then((res)=>setData(res.data))
-        .catch((err)=>console.log("catch error"+err))
-    },[])
-
-    const [Data] = useState(Store)
   return (
     <div>
         <Navbar/>
         <Routes>
-            <Route path='/' element={
-            <Data.Provider value={data}>
-               <Home/>
-            </Data.Provider>
-            } />
-            <Route path='/furniture' element={
-            <Data.Provider value={data}>
-                <Furniture/>
-            </Data.Provider>
-            } />
-            <Route path='/fashion' element={
-            <Data.Provider value={data}>
-               <Fashion/>     
-            </Data.Provider>
-            } />
-            <Route path='/mobile&tablet' element={
-                <Data.Provider value={data}>
-                    <MobileTablet/>
-                </Data.Provider>
-            } />
-            <Route path='/electronics' element={
-            <Data.Provider value={data}>
-               <Electronics/>     
-            </Data.Provider>
-            } />
-            
+            <Route path='/' element={<Home/>} />
+            <Route path='/furniture' element={<Furniture/>} />
+            <Route path='/fashion' element={<Fashion/>} />
+            <Route path='/mobile&tablet' element={<MobileTablet/> } />
+            <Route path='/electronics' element={<Electronics/>} />
             <Route path='/cart' element={<Cart/>} />
             <Route path='/authentication' element={<Authentication/>} />
-            <Route path='/rendercard/:id/:category' element={
-                <Data.Provider value={data}>
-                    <RenderCArd/>
-                </Data.Provider>
-            } />
-            <Route path='/subcomp/:subcategory/:subroutes/:heading' element={
-                <Data.Provider value={data}>
-                    <Subcomp/>
-                </Data.Provider>
-            } />
+            <Route path='/rendercard/:id/:category' element={<RenderCArd/>} />
+            <Route path='/subcomp/:subcategory/:subroutes/:heading' element={<Subcomp/>} />
             <Route path='/singup' element={<Singup/>} />
             <Route path='/signin' element={<Login/>} />
             <Route path='/searchcomp' element={<SearchRender/>} />
